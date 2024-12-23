e691415eea428f9bc6dc32dcd71dc590e15e8d05
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-23 21:50:53 +0530

    Show myFood by specific email

--- src/Components/Pages/MyFood.jsx
+++ src/Components/Pages/MyFood.jsx
@@ -1,10 +1,90 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import axios from "axios";
+import { GrDocumentUpdate } from "react-icons/gr";
+import { Link } from "react-router-dom";
+
 
 
 const MyFood = () => {
+
+    const {user} = useContext(AuthContext)
+    const [myFood, setMyFood] = useState([])
+    const [loading,setLoading] = useState(true)
+
+   useEffect(()=>{
+    if(user && user.email){
+        const myFood = async()=>{
+            try{
+ setLoading(true)
+ const response = await axios.get(`${import.meta.env.VITE_base_URL}/myPurchaseFood/${user.email}`)
+ setMyFood(response.data)
+            }catch(error){
+                  console.log(error);
+            }finally{
+                setLoading(false)
+            }
+        }
+        myFood()
+    }
+
+   },[user])
+
+   if(loading){
+    return <span className="loading loading-bars loading-lg"></span>
+   }
+
     return (
-        <div>
-            <h2>My Food</h2>
+        <div className="w-11/12 mx-auto">
+        <h2 className="text-2xl font-bold mb-4">My Food</h2>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {myFood.length === 0 ? (
+                <tr>
+                  <td colSpan="5" className="text-center">
+                    No foods added yet.
+                  </td>
+                </tr>
+              ) : (
+                myFood.map((food) => (
+                  <tr key={food._id}>
+              
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img
+                              src={food.foodImage}
+                              alt={food.name}
+                            />
+                          </div>
+                        </div>
+                        
+                      </div>
+                    </td>
+                    <td>{food.foodCategory}</td>
+                    <td>${food.price}</td>
+                    <th>
+                      <Link className="text-fuchsia-600 btn-md h-6"><GrDocumentUpdate /></Link>
+                    </th>
+                  </tr>
+                ))
+              )}
+            </tbody> 
+          </table>
         </div>
+      </div>
     );
 };
 
