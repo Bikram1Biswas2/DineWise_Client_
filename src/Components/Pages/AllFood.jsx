18c6e875d8145e2c46f399ed45f66cbf9291fcae
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-25 00:37:22 +0530

    search functionality completed

--- src/Components/Pages/AllFood.jsx
+++ src/Components/Pages/AllFood.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const AllFood = () => {
   const foods = useLoaderData();
+  const [search, setSearch] = useState("");
+  const [filteredFoods, setFilteredFoods] = useState(foods);
+
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase(); 
+    setSearch(query);
+
+    // Filter foods based on the search query
+    const filtered = foods.filter((food) =>
+      food.foodName.toLowerCase().includes(query)
+    );
+    setFilteredFoods(filtered);
+  };
 
   return (
     <div>
@@ -20,12 +34,38 @@ const AllFood = () => {
           </div>
         </div>
       </div>
+      {/* Search */}
+      <div className="w-6/12 mx-auto mt-5">
+        <label className="input input-bordered flex items-center gap-2">
+          <input
+            onChange={handleSearch}
+            type="text"
+            className="grow"
+            placeholder="Search"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </div>
+      {filteredFoods.length === 0 && (
+        <p className="text-center text-gray-500 mt-6">No food items found.</p>
+      )}
+      {/* Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-        {foods.map((food) => (
+        {filteredFoods.map((food) => (
           <div
             key={food._id}
-            className=" rounded-lg shadow-lg p-4 flex flex-col justify-between bg-[#bde8ca6a]"
-            //style={{ backgroundColor: '#BDE8CA' }}
+            className="rounded-lg shadow-lg p-4 flex flex-col justify-between bg-[#bde8ca6a]"
           >
             {/* Card Image */}
             <div className="w-full h-48 mb-4 overflow-hidden rounded-lg">
