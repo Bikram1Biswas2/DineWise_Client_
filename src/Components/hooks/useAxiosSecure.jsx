5dc5c14e97660a10ad53348f48f969bd5f6e839e
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-26 16:16:15 +0530

    some minor changes

--- src/Components/hooks/useAxiosSecure.jsx
+++ src/Components/hooks/useAxiosSecure.jsx
@@ -1,49 +1,31 @@
 import axios from "axios";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
-import { useNavigate } from "react-router-dom";
+
+
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_base_URL,
-  withCredentials: true,
-});
+      baseURL:import.meta.env.VITE_base_URL ,
+      withCredentials : true
+})
 
 const useAxiosSecure = () => {
-  const { logOut } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const interceptor = axiosInstance.interceptors.response.use(
-      (response) => {
-        return response; // Return response if there is no error
-      },
-      (error) => {
-        console.log("Error caught in interceptor:", error);
-
-        // Check for unauthorized or forbidden errors
-        // if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-        //     logOut()
-        //         .then(() => {
-        //             console.log("Logged out user");
-        //             navigate("/login"); // Redirect to login page
-        //         })
-        //         .catch(error => {
-        //             console.error("Error during sign-out:", error);
-        //         });
-        //}
-
-        return Promise.reject(error); // Reject the error to propagate it
-      }
-    );
-
-    // Cleanup the interceptor on component unmount
-    return () => {
-      axiosInstance.interceptors.response.eject(interceptor);
-    };
-  }, []);
-
-  return axiosInstance;
+      const {logout} = useContext(AuthContext)
+      useEffect(()=>{
+            axiosInstance.interceptors.response.use((response)=>{
+              return response
+            } , 
+            err => {
+                  
+                  // if (err.status === 401 || err.status === 403) {
+                  //       signOutUser()
+                  // }
+                  return Promise.reject(err)
+            }
+      )
+      },[logout])
+      return axiosInstance
 };
 
 export default useAxiosSecure;
